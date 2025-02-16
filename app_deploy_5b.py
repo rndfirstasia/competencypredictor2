c08731af68bc1d6abf1a4bfd0de6147a7062982f
rndfirstasia <158240722+rndfirstasia@users.noreply.github.com>
2025-02-16 13:00:13 +0700

    Update app_deploy_5b.py

--- app_deploy_5b.py
+++ app_deploy_5b.py
@@ -800,7 +800,7 @@ with tab1:
             try:
                 files = {'file': (file_name, audio_file_copy.getvalue(), 'audio/wav')}
                 data = {'registration_id': id_input_id_kandidat}
-		keepalive = {"Connection": "keep-alive"}
+		    keepalive = {"Connection": "keep-alive"}
                 response = requests.post(f"{flask_url}/transcribe", files=files, headers=keepalive, data=data, timeout=600)
                 
                 if response.status_code == 200:
